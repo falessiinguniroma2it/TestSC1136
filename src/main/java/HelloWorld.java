df65e0f845ba4067676ae678014ee103c51f32b0
falessiinguniroma2.it <falessi@ing.uniroma2.it>
2023-10-16 11:42:27 +0200

    Second commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, Davide!");
-        int a;
     }
 }
